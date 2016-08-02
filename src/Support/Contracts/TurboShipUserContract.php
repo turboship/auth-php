9670a4efa4732fbaf7eab150c5fa3669d867faf4
james weston <james@savannah.io>
2016-08-02 16:04:13 -0400

    Correcting function names to match trait

--- src/Support/Contracts/TurboShipUserContract.php
+++ src/Support/Contracts/TurboShipUserContract.php
@@ -6,6 +6,6 @@ namespace TurboShip\Auth\Support\Contracts;
 interface TurboShipUserContract
 {
     public function getId();
-    public function getTurboShipAuthAccount();
-    public function setTurboShipAuthAccount($account);
+    public function getTurboShipAccount();
+    public function setTurboShipAccount($account);
 }
